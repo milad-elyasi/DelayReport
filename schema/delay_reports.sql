2f0709a6db2766cc632e0be803f5f14e65bf3fd3
Milad Elyasi <milad.eliasi@snapp.doctor>
2023-03-11 11:21:30 +0330

    add db schema

---
+++ schema/delay_reports.sql
@@ -0,0 +1,3 @@
+insert into snappfood.delay_reports (id, order_id, eta, status, created_at, updated_at)
+values  (30, 1, null, 'QUEUE', '2023-03-11 01:47:42', '2023-03-11 01:47:42'),
+        (31, 3, null, 'QUEUE', '2023-03-11 09:52:44', '2023-03-11 09:52:44');
